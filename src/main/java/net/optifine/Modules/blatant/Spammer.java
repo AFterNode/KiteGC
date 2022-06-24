bb9f5d723de788e8dad506ee934eab96936d443a
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2022-06-24 09:16:21 +0800

    Spammer支持花雨庭公屏发送

--- src/main/java/net/optifine/Modules/blatant/Spammer.java
+++ src/main/java/net/optifine/Modules/blatant/Spammer.java
@@ -6,16 +6,21 @@ import net.optifine.Modules.Module;
 import net.optifine.Modules.ModuleType;
 import net.optifine.Utils.Helper;
 import net.optifine.Utils.SpammerThread;
+import net.optifine.Values.Option;
 import org.lwjgl.input.Keyboard;
+import scala.tools.cmd.gen.AnyValReps;
 
 public class Spammer extends Module {
     private static String content = "KiteGC >> https://afternode.cn/ << VapuLite based";
     public static boolean status;
     private static SpammerThread thread;
+    public static Option<Boolean> HytPublic = new Option<>("HytPublic", "HytPublic", false);
 
     public Spammer() {
         super("Spammer", Keyboard.KEY_NONE, ModuleType.Blatant, "Auto send message");
         Chinese = "刷屏";
+
+        addValues(HytPublic);
     }
 
     public static void setContent(String c) {
--- src/main/java/net/optifine/Utils/SpammerThread.java
+++ src/main/java/net/optifine/Utils/SpammerThread.java
@@ -6,10 +6,16 @@ import java.util.concurrent.TimeUnit;
 
 public class SpammerThread extends Thread{
     public void run() {
+        StringBuilder builder = new StringBuilder();
+        if (Spammer.HytPublic.getValue()) {
+            builder.append("@");
+        }
+        builder.append(Spammer.getContent());
+
         while (true) {
             try {
                 if (!Spammer.status) break;
-                Spammer.mc.thePlayer.sendChatMessage(Spammer.getContent());
+                Spammer.mc.thePlayer.sendChatMessage(builder.toString());
                 TimeUnit.SECONDS.sleep(1);
             } catch (Exception e) {
                 System.out.println("[Kite] Exception occurred at SpammerThread: " + e.getMessage());
