6bf99a25dfe23fffedc7007e253aa7bb62ab160d
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2022-06-24 09:13:43 +0800

    Hitokoto支持花雨庭公屏发送

--- src/main/java/net/optifine/Modules/message/Hitokoto.java
+++ src/main/java/net/optifine/Modules/message/Hitokoto.java
@@ -4,6 +4,7 @@ import net.optifine.Modules.Module;
 import net.optifine.Modules.ModuleType;
 import net.optifine.Utils.utils.Helper;
 import net.optifine.Values.Mode;
+import net.optifine.Values.Option;
 import org.lwjgl.input.Keyboard;
 
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ import java.util.Random;
 
 public class Hitokoto extends Module {
     private Mode<Enum> from = new Mode<>("Mode", "mode", modes.values(), modes.HitokotoAPI);
+    private Option<Boolean> HytPublic = new Option<>("HytPublic", "HytPublic", false);
     private final List<HashMap<String, String >> maps = new ArrayList<>();
 
     enum modes {
@@ -23,7 +25,7 @@ public class Hitokoto extends Module {
     public Hitokoto() {
         super("Hitokoto", Keyboard.KEY_NONE, ModuleType.Message, "Get sentence from HitokotoAPI and send it");
         Chinese = "一言";
-        addValues(from);
+        addValues(from, HytPublic);
 
         {
             HashMap<String, String> tmp =  new HashMap<>();
@@ -42,8 +44,11 @@ public class Hitokoto extends Module {
             } else if (m == modes.Internal_Beta) {
                 Random random = new Random();
                 HashMap<String, String> hashMap = maps.get(random.nextInt(maps.toArray().length));
-                mc.thePlayer.sendChatMessage(hashMap.get("hitokoto")
-                        + " | 来自：" + hashMap.get("from") + ">>Kite Hitokoto<<");
+                StringBuilder builder = new StringBuilder();
+                if (HytPublic.getValue()) builder.append("@");
+                builder.append(hashMap.get("hitokoto")).append(" | 来自：").append(hashMap.get("from")).append(" >>KITE HITOKOTO<<");
+
+                mc.thePlayer.sendChatMessage(builder.toString());
             }
         } catch (Exception e) {
             Helper.sendMessage("Exception occurred: " + e.getMessage());
