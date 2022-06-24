d8fced73da5cd632338bf5ec0f5fae5168c17184
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2022-06-24 09:34:01 +0800

    尝试修复MurderMystery只检测一次的问题

--- src/main/java/net/optifine/Modules/world/MurderMystery.java
+++ src/main/java/net/optifine/Modules/world/MurderMystery.java
@@ -6,6 +6,7 @@ import net.optifine.Modules.ModuleType;
 import net.optifine.Modules.Module;
 import net.optifine.Modules.combat.AntiBot;
 import net.optifine.Utils.utils.Helper;
+import net.optifine.Values.Numbers;
 import net.optifine.Values.Option;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.player.EntityPlayer;
@@ -16,11 +17,13 @@ import net.minecraft.item.ItemSword;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.lwjgl.input.Keyboard;
+import scala.Array;
 import scala.tools.cmd.gen.AnyValReps;
 
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class MurderMystery extends Module {
 
@@ -29,9 +32,11 @@ public class MurderMystery extends Module {
     private static List<EntityPlayer> m;
     private static List<EntityPlayer> bw;
 
-    private Option<Boolean> Murder = new Option<Boolean>("Tell Everyone Murder","Tell Everyone Murder", false);
-    private Option<Boolean> Bow= new Option<Boolean>("Tell Everyone Bow","Tell Everyone Bow", false);
-    private Option<Boolean> Hyt = new Option<>("[BETA] HytExtension", "hytExtension", false);
+    public static Option<Boolean> Murder = new Option<Boolean>("Tell Everyone Murder","Tell Everyone Murder", false);
+    public static Option<Boolean> Bow= new Option<Boolean>("Tell Everyone Bow","Tell Everyone Bow", false);
+    public static Option<Boolean> Hyt = new Option<>("HytExtension", "hytExtension", false);
+    public static Numbers<Integer> delay = new Numbers<>("AutoDetectDelay", "AutoDetectDelay", 5, 10, 1, 1);
+    public static boolean status = false;
 
     public MurderMystery() {
         super("MurderMystery", Keyboard.KEY_NONE, ModuleType.World,"Detection Murders in Murder game");
@@ -39,11 +44,19 @@ public class MurderMystery extends Module {
         Chinese="杀手检查";
     }
 
+    @Override
+    public void enable() {
+        status = true;
+
+        MMThread thread = new MMThread();
+        thread.start();
+    }
 
     @Override
     public void disable() {
         MurderMystery.a1 = false;
         MurderMystery.a2 = 0;
+        status = false;
     }
 
     @SubscribeEvent
@@ -94,7 +107,7 @@ public class MurderMystery extends Module {
         }
     }
 
-    private void c() {
+    public static void c() {
         if (MurderMystery.m.size() > 0) {
             MurderMystery.m.clear();
         }
@@ -110,6 +123,68 @@ public class MurderMystery extends Module {
         MurderMystery.bw = new ArrayList<EntityPlayer>();
     }
 
+    public static List<EntityPlayer> getM() { return MurderMystery.m; }
+    public static List<EntityPlayer> getBw() { return bw; }
 
+}
 
+class MMThread extends Thread {
+    public void detect() {
+        if (MurderMystery.mc.thePlayer.getWorldScoreboard() != null && MurderMystery.mc.thePlayer.getWorldScoreboard().getObjectiveInDisplaySlot(1) != null) {
+            final String d = MurderMystery.mc.thePlayer.getWorldScoreboard().getObjectiveInDisplaySlot(1).getDisplayName();
+            if (d.contains("MURDER") || d.contains("MYSTERY")) {
+                for (final EntityPlayer en : MurderMystery.mc.theWorld.playerEntities) {
+                    if (en != MurderMystery.mc.thePlayer && !en.isInvisible() && !AntiBot.isServerBot(en)) {
+                        if (en.getHeldItem() != null && en.getHeldItem().hasDisplayName()) {
+                            final Item i = en.getHeldItem().getItem();
+                            if (i instanceof ItemSword || i instanceof ItemAxe || en.getHeldItem().getDisplayName().replaceAll("§", "").equals("aKnife")) {
+                                if (!MurderMystery.getM().contains(en)) {
+                                    MurderMystery.getM().add(en);
+                                    MurderMystery.mc.thePlayer.playSound("note.pling", 1.0f, 1.0f);
+                                    Helper.sendMessage(en.getName() + " 手中检测到武器");
+                                    if(MurderMystery.Murder.getValue()){
+                                        MurderMystery.mc.thePlayer.sendChatMessage(en.getName() + " 是狼");
+                                    }
+                                }
+                            }
+                            else if (i instanceof ItemBow && !MurderMystery.getBw().contains(en)) {
+                                MurderMystery.getBw().add(en);
+                                Helper.sendMessage("[WARNING]" + en.getName() + " have bow! he maybe will kill you.");
+                                if(MurderMystery.Bow.getValue()){
+                                    MurderMystery.mc.thePlayer.sendChatMessage(en.getName() + " have bow.");
+                                }
+                            } else if (i == Items.blaze_rod && !MurderMystery.getBw().contains(en)) {
+                                Helper.sendMessage("[WARNING] " + en.getName() + " 是预言家");
+                            }
+                        }
+                        int rgb = Color.green.getRGB();
+                        if ((MurderMystery.getM().contains(en) && !MurderMystery.getBw().contains(en)) || (MurderMystery.getM().contains(en) && MurderMystery.getBw().contains(en))) {
+                            rgb = Color.red.getRGB();
+                        }
+                        if (!MurderMystery.getM().contains(en) && MurderMystery.getBw().contains(en)) {
+                            rgb = Color.orange.getRGB();
+                        }
+                    }
+                }
+            }
+            else {
+                MurderMystery.c();
+            }
+        }
+        else {
+            MurderMystery.c();
+        }
+    }
+
+    public void run() {
+        while (MurderMystery.status) {
+            try {
+                detect();
+                TimeUnit.SECONDS.sleep(MurderMystery.delay.getValue());
+            } catch (Exception e) {
+                System.out.println("[Kite] Exception occurred at MurderMystery: " + e.getMessage());
+                e.printStackTrace();
+            }
+        }
+    }
 }
