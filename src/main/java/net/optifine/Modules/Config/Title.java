87f2a5d497934969309bf5da7dccf94ef0d2fdea
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2022-06-27 13:16:24 +0800

    暂时不要使用此模块，会导致客户端卡死

--- src/main/java/net/optifine/Modules/Config/Title.java
+++ src/main/java/net/optifine/Modules/Config/Title.java
@@ -10,19 +10,30 @@ import org.lwjgl.opengl.Display;
 import java.util.Map;
 
 public class Title extends Module {
+    String original;
+
     public Title() {
         super("Title Changer", Keyboard.KEY_NONE, ModuleType.Config, "更改游戏窗口标题");
+        original = Display.getTitle();
     }
 
     @Override
     public void enable() {
-        Thread hitokoto = new Thread() {
-            public void run() {
-                String get = Hitokoto.get("a");
-                Map map = Hitokoto.parse(get);
-                String content = (String) map.get("hitokoto");
-                Display.setTitle(Client.name + " " + Client.version + " | " + content);
-            }
-        };
+        Thread hitokoto = new ChangeTitle();
+        hitokoto.start();
+    }
+
+    @Override
+    public void disable() {
+        Display.setTitle(original);
+    }
+}
+
+class ChangeTitle extends Thread {
+    public void run() {
+        String get = Hitokoto.get("a");
+        Map map = Hitokoto.parse(get);
+        String content = (String) map.get("hitokoto");
+        Display.setTitle(Client.name + " " + Client.version + " | " + content);
     }
 }
