f7ed0b527076224441633ae863739752a0e8b88d
tumuidle <7367204+tumuidle@user.noreply.gitee.com>
2022-06-29 13:12:56 +0800

    取消停止支持

--- src/main/java/net/optifine/Modules/world/FastPlace.java
+++ src/main/java/net/optifine/Modules/world/FastPlace.java
@@ -6,10 +6,12 @@ import net.optifine.Utils.utils.TimerUtil;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.optifine.Values.Numbers;
 import org.lwjgl.input.Keyboard;
 
 public class FastPlace extends Module {
     private final TimerUtil timer = new TimerUtil();
+    private Numbers<Integer> delay = new Numbers<>("Delay(MS)", "Delay", 20, 10, 1000, 1);
     public FastPlace() {
         super("FastPlace", Keyboard.KEY_NONE, ModuleType.World,"Make you place the blocks faster");
         Chinese="快速放置";
@@ -20,7 +22,7 @@ public class FastPlace extends Module {
         int key = mc.gameSettings.keyBindUseItem.getKeyCode();
 
         if (mc.gameSettings.keyBindUseItem.isKeyDown()) {
-                if (timer.delay(20)) {
+                if (timer.delay(delay.getValue())) {
                     mc.thePlayer.swingItem();
                     KeyBinding.onTick(key);
                     mc.thePlayer.getHeldItem().useItemRightClick(mc.theWorld, mc.thePlayer);
